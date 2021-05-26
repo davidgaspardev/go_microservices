55406ecee2fe52751bbd98354eff249f287b2d38
David Gaspar <davidgaspar.dev@gmail.com>
2021-05-26 17:29:49 -0400

    Create server mux

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
+    const message = "Hello, I'm using Golang";
+
+    // Creating a server mux
+    mux := http.NewServeMux();
+    mux.HandleFunc("/", func(w http.ResponseWriter, r * http.Request) {
+      	w.WriteHeader(http.StatusOK);
+        w.Write([]byte(message));
+    });
+
+    http.ListenAndServe(":8080", mux);
+
     fmt.Println("Hello World!");
 }
