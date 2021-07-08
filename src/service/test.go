1f8af22abba7c342f33a35fbbf29dfdeefea1a6e
David Gaspar <davidgaspar.dev@gmail.com>
2021-07-08 14:15:26 -0400

    Code commented

--- src/service/test.go
+++ src/service/test.go
@@ -33,3 +33,34 @@ func (service *Service) Run() {
 		log.Fatalf("Server dailed to start: %v", err)
 	}
 }
+
+// func NewServer(mux *http.ServeMux) *http.Server {
+// 	tlsConfig := &tls.Config{
+// 		// See more here https://www.namecheap.com/blog/beginners-guide-to-tls-cipher-suites/
+// 		PreferServerCipherSuites: false,
+// 		CurvePreferences: []tls.CurveID{
+// 			tls.CurveP256,
+// 			tls.X25519, // Go 1.8 only
+// 		},
+// 		MinVersion: tls.VersionTLS12,
+// 		CipherSuites: []uint16{
+// 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+// 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+// 			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+// 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+// 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+// 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+// 		},
+// 	}
+
+// 	server := &http.Server{
+// 		Addr:         ":8080",
+// 		ReadTimeout:  5 * time.Second,
+// 		WriteTimeout: 10 * time.Second,
+// 		IdleTimeout:  120 * time.Second,
+// 		TLSConfig:    tlsConfig,
+// 		Handler:      mux,
+// 	}
+
+// 	return server
+// }
